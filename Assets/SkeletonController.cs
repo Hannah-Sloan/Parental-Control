7f9843a9713cd7a07657e50af25df6b38f69943a
Hannah-Sloan <hannah.ava.sloan@gmail.com>
2023-07-09 02:01:41 -0600

    Althea Enemies!

---
+++ Assets/SkeletonController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SkeletonController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private new Rigidbody2D rigidbody;
+    [SerializeField] private SpriteRenderer renderer;
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        var direction = Mathf.Sign(((target.position - transform.position).normalized).x);
+        renderer.flipX = direction > 0;
+        rigidbody.velocity = new Vector2(direction * speed, rigidbody.velocity.y);
+    }
+}
